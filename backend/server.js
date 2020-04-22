5e5de8b9a895669d7c3cb254c383d11d342e1398
Sarayu Gautam <sarayugautam1@gmail.com>
2020-04-22 18:30:41 +0545

    Connected To MongoDB using Mongoose Changes to be committed: 	modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+//setting Up Database Connection
+mongoose.connect("mongodb://127.0.0.1:27017/todos", {
+  useNewUrlParser: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(PORT, function () {
   console.log("Server running on PORT:" + PORT);
